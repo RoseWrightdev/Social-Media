77b0540c4709ca9b8c3dc95242ffd9b3a1f6f017
Rose Wright <rosesophiewright@gmail.com>
2024-04-14 16:41:05 -0500

    added github.com/joho/godotenv

--- backend/go/backend/database/database.go
+++ backend/go/backend/database/database.go
@@ -1,32 +1,40 @@
 package database
 
 import (
-    "database/sql"
-    "fmt"
-    _ "github.com/lib/pq"
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func Connect() (*sql.DB, error) {
-    var (
-        host     = "localhost"
-        port     = "5432"
-        user     = "postgres"
-        password = ""
-        dbname   = ""
-    )
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
 
-    connString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-        host, port, user, password, dbname)
+	var (
+		host     = os.Getenv("HOST")
+		port     = os.Getenv("PORT")
+		user     = os.Getenv("USER")
+		password = os.Getenv("PASSWORD")
+		dbname   = os.Getenv("DB_NAME")
+	)
 
-    db, err := sql.Open("postgres", connString)
-    if err != nil {
-        return nil, err
-    }
+	connString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
 
-    err = db.Ping()
-    if err != nil {
-        return nil, err
-    }
+	db, err := sql.Open("postgres", connString)
+	if err != nil {
+		return nil, err
+	}
 
-    return db, nil
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- backend/go/backend/database/go.mod
+++ backend/go/backend/database/go.mod
@@ -3,3 +3,5 @@ module backend/database
 go 1.22.2
 
 require github.com/lib/pq v1.10.9
+
+require github.com/joho/godotenv v1.5.1
