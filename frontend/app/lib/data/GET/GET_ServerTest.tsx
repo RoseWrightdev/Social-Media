39f63e472537e3bf366d898c48bd9308e044fe7c
Rose Wright <rosesophiewright@gmail.com>
2024-04-15 12:11:33 -0500

    add typing to server test

--- frontend/app/lib/data/GET/GET_ServerTest.tsx
+++ frontend/app/lib/data/GET/GET_ServerTest.tsx
@@ -1,4 +1,5 @@
 import { SERVER_PATH } from '@/app/lib/utils/constants'
+import { GET_SeverTest_TYPE } from '@/app/lib/utils/types'
 
 async function getData() {
   const res = await fetch(SERVER_PATH + "/test")
@@ -12,7 +13,7 @@ async function getData() {
 
 export default async function GET_ServerTest() {
   const dataArr = await getData()
-  const data = dataArr[0]
+  const data: GET_SeverTest_TYPE = dataArr[0]
   return (
     <>
       <h1>Go Server</h1>
