a37f134b7c65541bbcc5b37a0c0b47c423529af5
Rose Wright <rosesophiewright@gmail.com>
2024-04-28 22:15:00 -0500

    add login button

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,25 @@
-import GET_ServerTest from "@/app/lib/data/GET/GET_ServerTest"
-import GET_Database from "@/app//lib/data/GET/GET_Database"
 
 export default function Home(){
-
   return (
     <>
-      <GET_ServerTest/>
-      <GET_Database/>
+      <div className="flex">
+        {/* left desktop only */}
+        <div className="bg-slate-950 w-[50dvw] h-[100dvh] hidden md:flex">
+
+        </div>
+        {/* right */}
+        <div className="flex flex-col w-[100dvw] md:w-[50dvw]">
+          <button className="place-self-end p-6 border-2">Login</button>
+          <div className="flex-col items-center justify-center text-center">
+            <div>
+              <h1 className="font-black text-3xl">Create an account</h1>
+              <h3>Enter your email below to create your account</h3>
+            </div>
+          </div>
+        </div>
+        {/* sign-up */}
+        
+      </div>
     </>
   )
 }
