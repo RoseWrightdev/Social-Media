ac07dad6ac83e325f529e6dbeb85c9ae55ceb74b
Rose Wright <rosesophiewright@gmail.com>
2024-05-04 20:23:44 -0500

    JSON bug fix

--- backend/go/get.go
+++ backend/go/get.go
@@ -26,27 +26,16 @@ func GetDataBaseTest(c *gin.Context) {
 	if err != nil {
 		panic(err)
 	}
+	defer rows.Close()
 
+	var users []UsersSchema
 	for rows.Next() {
-		var id string
-		var username string
-		var email string
-		var password string
-
-		err := rows.Scan(&id, &username, &email, &password)
+		var user UsersSchema
+		err := rows.Scan(&user.Id, &user.Username, &user.Email, &user.Password)
 		if err != nil {
 			panic(err)
 		}
-
-		var data = []UsersSchema{
-			{
-				Id:       id,
-				Username: username,
-				Email:    email,
-				Password: password,
-			},
-		}
-
-		c.IndentedJSON(http.StatusOK, data)
+		users = append(users, user)
 	}
+	c.IndentedJSON(http.StatusOK, users)
 }
