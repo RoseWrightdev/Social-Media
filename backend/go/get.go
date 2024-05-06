9b2189df312d32dd393aa2b6cd1868220e47c310
Rose Wright <rosesophiewright@gmail.com>
2024-05-05 22:47:31 -0500

    Add Limit of 10 & naming convention  change

--- backend/go/get.go
+++ backend/go/get.go
@@ -10,19 +10,19 @@ var portStatus = []PortAndStatus{
 	{Port: "8000", Status: http.StatusOK},
 }
 
-func GetServerTest(c *gin.Context) {
+func GetServer(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, portStatus)
 }
 
 
-func GetDataBaseTest(c *gin.Context) {
+func GetDataBase(c *gin.Context) {
 	db, err := Connect()
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT * FROM users")
+	rows, err := db.Query("SELECT * FROM users LIMIT 10")
 	if err != nil {
 		panic(err)
 	}
@@ -38,4 +38,28 @@ func GetDataBaseTest(c *gin.Context) {
 		users = append(users, user)
 	}
 	c.IndentedJSON(http.StatusOK, users)
+}
+
+func GetUserById(c *gin.Context) {
+	db, err := Connect()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	id := c.Param("id")
+	rows, err := db.Query("SELECT * FROM users WHERE id = $1", id)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var user UsersSchema
+	for rows.Next() {
+		err := rows.Scan(&user.Id, &user.Password, &user.Email, &user.Username)
+		if err != nil {
+			panic(err)
+		}
+	}
+	c.IndentedJSON(http.StatusOK, user)
 }
