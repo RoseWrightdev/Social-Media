e1d08c8f22d69968fbfb443fc2c4e9484234dad7
Rose Wright <rosesophiewright@gmail.com>
2024-05-05 22:48:15 -0500

    Comments added & GetUserById route handling

--- backend/go/server.go
+++ backend/go/server.go
@@ -17,12 +17,29 @@ func Run() {
 	router.Use(CORSMiddleware())
 
 	//Get
-	router.GET("/test", GetServerTest)
-	router.GET("/database", GetDataBaseTest)
-	//router.GET("/database/:id", GetDataBaseTestById)
+	router.GET("/server", GetServer)
+	router.GET("/database", GetDataBase)
+	router.GET("/user/:id", GetUserById)
+
+	// TODO: Add the following routes:
+	//router.GET("/post/:id", GetPostById)
+	//router.GET("/posts", GetPosts)
+	//router.GET("/user/:id/posts", GetPostsByUserId)
+	//router.GET("/user/:id/posts/:post_id", GetPostByUserIdAndPostId)
+
+	//Put router.PUT("/user/:id", PutUserById)
+	//Put router.PUT("/post/:id", PutPostById)
+	//Put router.PUT("/user/:id/post/:post_id", PutPostByUserIdAndPostId)
+
+	//Delete router.DELETE("/user/:id", DeleteUserById)
+	//Delete router.DELETE("/post/:id", DeletePostById)
+	//Delete router.DELETE("/user/:id/post/:post_id", DeletePostByUserIdAndPostId)
+	//Delete router.DELETE("/user/:id/posts", DeletePostsByUserId)
 
 	//Post
+
 	router.POST("/register", PostRegister)
+	
 	router.Run("localhost:8080")
 }
 
