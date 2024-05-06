d6e65a6fa57661acfcc6482ceac6ef7ed30a8af4
Rose Wright <rosesophiewright@gmail.com>
2024-05-05 22:46:47 -0500

    Error handling

--- backend/go/post.go
+++ backend/go/post.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
 func PostRegister(c *gin.Context) {
@@ -16,12 +17,21 @@ func PostRegister(c *gin.Context) {
 	var user UsersSchema
 	c.BindJSON(&user)
 
-	_, err = db.Exec("INSERT INTO users (username, email, password) VALUES ($1, $2, $3)", user.Username, user.Email, user.Password);
-		if err != nil {
-		panic(err)
+	_, err = db.Exec("INSERT INTO users (username, email, password) VALUES ($1, $2, $3)", user.Username, user.Email, user.Password)
+	if err != nil {
+			if pqErr, ok := err.(*pq.Error); ok {
+					switch pqErr.Code {
+					case "23505":
+							c.IndentedJSON(http.StatusConflict, gin.H{
+									"message": "Username or email already in exists",
+							})
+							return
+					}
+			}
+			panic(err)
 	}
 
 	c.IndentedJSON(http.StatusOK, gin.H{
-		"message": "User created",
+			"message": "User created",
 	})
 }
