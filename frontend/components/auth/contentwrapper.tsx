c1fad8bf0363855afbb3bcf420b1d8f806e9f216
Rose Wright <rosesophiewright@gmail.com>
2024-05-20 16:35:37 -0500

    update content wrapper comments

--- frontend/components/auth/contentwrapper.tsx
+++ frontend/components/auth/contentwrapper.tsx
@@ -5,11 +5,10 @@ export default function ContentWrapper({children}: {children: React.ReactNode}) 
         {/* left desktop only */}
         <div className="bg-slate-950 w-[50dvw] h-[100dvh] hidden md:flex">
         </div>
-        {/* right */}
+        {/* right mobile & desktop */}
         <div className="flex flex-col items-center justify-center w-[100dvw] md:w-[50dvw] h-[100dvh] ">
           {children}
         </div>
-        {/* sign-up */}
       </div>
     </>
   )
