5e11183ef77839eda79333a0a839a1f860d12b2f
Rose Wright <rosesophiewright@gmail.com>
2024-06-21 12:52:40 -0500

    remove old type

--- frontend/lib/types.ts
+++ frontend/lib/types.ts
@@ -1,10 +1,3 @@
-export interface GET_Database_TYPE {
-  id: string,
-  username: string,
-  email: string,
-  password: string
-}
-
 export interface User_TYPE {
   id: string, 
   username: string, 
