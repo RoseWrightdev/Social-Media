1c3b98afac735a782f3e521f9e58873799179823
Rose Wright <rosesophiewright@gmail.com>
2024-07-02 10:37:55 -0500

    base64 encoding

--- scripts/seed/seed.py
+++ scripts/seed/seed.py
@@ -1,4 +1,5 @@
 import asyncio
+import base64
 import os
 from dotenv import load_dotenv
 import httpx
@@ -9,17 +10,21 @@ async def post_content(path, parent_id, contentType, url):
             try:
                 file_path = os.path.join(path, filename)
                 text_content = f'This is text content. #{index}'
-                files = {'file': (filename, open(file_path, 'rb'))}
-                data = {'id': parent_id, 'text': text_content, 'type': contentType}
-                print(f"Sending data: {data}")
-                r = await client.post(url, files=files, data=data)
+                with open(file_path, 'rb') as f:
+                    file_data = f.read()
+                    encoded_data = base64.b64encode(file_data).decode('utf-8')
+                data = {
+                    "id": parent_id,
+                    "text": text_content,
+                    "type": contentType,
+                    "file": encoded_data
+                }
+                r = await client.post(url, json=data)
                 print(r.text)
                 print('\n')
             except Exception as e:
                 print(f"Error sending file {filename}: {e}")
                 print('\n')
-            finally:
-                files['file'][1].close()
 
 async def main():
     load_dotenv()
@@ -27,8 +32,6 @@ async def main():
     url = os.getenv('URL')
     photos_path = os.path.join(os.path.dirname(__file__), 'photos')
     videos_path = os.path.join(os.path.dirname(__file__), 'videos')
-    url = 'http://localhost:8080/posts'
-
     await post_content(photos_path, parent_id, 'photos', url)
     await post_content(videos_path, parent_id, 'videos', url)
 
