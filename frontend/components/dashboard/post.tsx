bd1d599ac443c28e9e110f99177cc62b59b8b836
Rose Wright <rosesophiewright@gmail.com>
2024-07-23 17:09:36 -0500

    lowercase usernames and no italics

--- frontend/components/dashboard/post.tsx
+++ frontend/components/dashboard/post.tsx
@@ -12,7 +12,7 @@ export default function Post({textContent, postID, parentID}: PostData) {
       <div className="mb-8">
         <div className="flex">
           <Pfp userId={parentID} />
-          <h3 className="italic font-thin text-slate-400 my-auto ml-4">@{<Username userId={parentID}/>}</h3>
+          <h3 className="text-slate-400 my-auto ml-2 lowercase">@{<Username userId={parentID}/>}</h3>
         </div>
         <br />
           <div className="ml-2">
