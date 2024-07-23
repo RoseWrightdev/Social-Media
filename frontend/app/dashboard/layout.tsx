a24409d3aa1b26fc3d3a069ff29e4766d7a88729
Rose Wright <rosesophiewright@gmail.com>
2024-07-22 21:50:49 -0500

    layout logo height but fix

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -2,21 +2,23 @@ import { Logo, ProfilePicture, Settings } from "@/components/dashboard/icons"
 
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
-    <div className="flex flex-row h-[100dvh]">
-      <div className="flex flex-col w-[25dvw] items-end p-2 pt-12">
-        <div className="sticky top-12">
-          <Logo/>
+    <>
+      <div className="flex flex-row">
+        <div className="flex flex-col w-[25dvw] items-end p-2 pt-12">
+          <div className="sticky top-12">
+            <Logo/>
+          </div>
+          <div className="sticky top-32">
+            <ProfilePicture />
+          </div>
+          <div className="sticky top-52">
+            <Settings/>
+          </div>
         </div>
-        <div className="sticky top-32">
-          <ProfilePicture />
-        </div>
-        <div className="sticky top-52">
-          <Settings/>
+          <div className="w-[50dvw] pt-12 mx-8">{children}</div>
+          <div className="flex flex-col w-[25dvw] pt-12">
         </div>
       </div>
-        <div className="w-[50dvw] pt-12 mx-8">{children}</div>
-        <div className="flex flex-col w-[25dvw] pt-12">
-      </div>
-    </div>
+    </>
   )
 }
