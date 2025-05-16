1961b5cf2688446257a8c043835ca19e9936a78c
RoseWrightdev <rosesophiewright@gmail.com>
2025-05-16 17:32:07 -0500

    file structure

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,5 +1,9 @@
 import { LoginForm } from "@/components/login-form"
 
+/**
+ * 
+ * @returns 
+ */
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
