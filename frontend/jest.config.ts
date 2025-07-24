e79aee4fd73a226f85056035c83a4e77420314e9
RoseWrightdev <rosesophiewright@gmail.com>
2025-07-24 16:22:01 -0500

    zustand + jest mocking

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'jest'
 import nextJest from 'next/jest.js'
- 
+
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
@@ -11,7 +11,7 @@ const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['./setup-jest.ts'],
 }
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
