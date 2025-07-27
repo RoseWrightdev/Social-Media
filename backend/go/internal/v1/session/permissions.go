e69edbae0aaeac26ba0fc943e9e0779f72c7cb7f
RoseWrightdev <rosesophiewright@gmail.com>
2025-07-27 18:04:01 -0500

    add docs

--- backend/go/internal/v1/session/permissions.go
+++ backend/go/internal/v1/session/permissions.go
@@ -2,8 +2,9 @@ package session
 
 import "k8s.io/utils/set"
 
-// todo: add docs
-// todo: add file level comment
+// permission.go defines the permission structure. These functions are to be
+// used in combination with (r *Room) broadcast (r *Room) broadcastRoomState
+// to describe which roles have permission to view the message.
 
 // HasWaitingPermission returns a set containing the RoleTypeWaiting role.
 // This can be used to check if a user has the "waiting" permission.
@@ -11,14 +12,16 @@ func HasWaitingPermission() set.Set[RoleType] {
 	return set.New(RoleTypeWaiting)
 }
 
-// HasParticipantPermission returns a set of RoleType values that are granted participant-level permissions.
-// The set includes roles such as Host, Screenshare, and Participant.
+// HasParticipantPermission returns a set of RoleType values that are granted
+// participant-level permissions. The set includes roles such as Host, 
+// Screenshare, and Participant.
 func HasParticipantPermission() set.Set[RoleType] {
 	return set.New(RoleTypeHost, RoleTypeScreenshare, RoleTypeParticipant)
 }
 
-// HasScreensharePermission returns a set of RoleType values that are permitted to use the screenshare feature.
-// The set typically includes roles such as Host and Screenshare, granting them the necessary permissions.
+// HasScreensharePermission returns a set of RoleType values that are 
+// permitted to use the screenshare feature. The set typically includes roles
+// such as Host and Screenshare, granting them the necessary permissions.
 func HasScreensharePermission() set.Set[RoleType] {
 	return set.New(RoleTypeHost, RoleTypeScreenshare)
 }
